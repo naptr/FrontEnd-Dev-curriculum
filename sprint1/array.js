e96b4782dedd139ce3e13968774f37bf400d7008
ziunadev <krishna.putra332@gmail.com>
2021-01-29 18:01:11 +0700

    edit some codes

---
+++ sprint1/array.js
@@ -0,0 +1,27 @@
+// kosongkan variabel array1 menggunakan looping for dan function pop()
+// contoh: array1.pop();
+
+var array1 = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+
+// for (;;) {}
+// seharusnya menampilkan "[]" pada terminal
+console.log(array1);
+
+
+// masukkan setiap element pada variabel array3
+// kedalam variabel array2.
+// Gunakan index untuk mendapat value
+// dari setiap element array
+// contoh: array3[index];
+// dan gunakan looping untuk memasukkan setiap element
+// dan juga jangan lupa gunakan function push
+// contoh: array2.push(...);
+
+var array2 = [0,1,2];
+var array3 = [9,8,7,6,5,4,3,2,1];
+// console.log(array3.length);
+
+// for(;;) {}
+// seharusnya menampilkan "[0,1,2,9,8,7,6,5,4,3,2,1]" pada terminal
+console.log(array2);
---
+++ sprint1/topiknya/topik5_Array/test.js
@@ -0,0 +1,8 @@
+var n = 9;
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(arr.length);
+
+for (let i = 0; i < 9; i++) {
+  arr.pop();
+}
