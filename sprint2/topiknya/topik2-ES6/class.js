886afd153e82a7ef1ea129efdc797e0567e65596
ziunadev <krishna.putra332@gmail.com>
2021-02-03 08:39:10 +0700

    update

--- sprint2/topiknya/topik2-ES6/class.js
+++ sprint2/topiknya/topik2-ES6/class.js
@@ -1,28 +1,66 @@
-// Class
-// equivalent to creating object using constructor function
+// // Class
+// // equivalent to creating object using constructor function
+//
+// class Person {  // Creating object
+//   constructor(name) { // initialized once the class called
+//     this.name = name;
+//   }
+//
+//   talk() {   // declaring function inside class
+//     console.log(`ini ${this.name} dari class Person`);
+//   }
+// }
+//
+// var person = new Person("juan");
+//
+// person.talk();
+//
+//
+// function Person2(string) {
+//   this.string = string;
+//   this.func = function () { // declaring function inside constructor function
+//     console.log(`ini ${string} dari constructor function`);
+//   }
+// }
+//
+// var person1 = new Person2("juan");
+//
+// person1.func();
 
-class Person {  // Creating object
-  constructor(name) { // initialized once the class called
+
+class Animal {
+  constructor(name, age) {
     this.name = name;
+    this.age = age;
   }
 
-  talk() {   // declaring function inside class
-    console.log(`ini ${this.name} dari class Person`);
-  }
-}
-
-var person = new Person("juan");
-
-person.talk();
-
-
-function Person2(string) {
-  this.string = string;
-  this.func = function () { // declaring function inside constructor function
-    console.log(`ini ${string} dari constructor function`);
+  info() {
+    console.log(`Saya adalah hewan ${this.name}`);
+    console.log(`Usia saya ${this.age}`);
   }
 }
 
-var person1 = new Person2("juan");
+var anjing = new Animal("Anjing", 12);
+anjing.info();
 
-person1.func();
+
+// Constructor Overriding concept in JavaScript
+class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age); // overriding using super()
+
+    this.breed = breed; // new property
+  }
+
+  info() {
+    super.info();
+    // console.log(`Saya adalah hewan ${this.name}`);
+    // console.log(`Usia saya ${this.age}`);
+
+    // new method value
+    console.log(`Jenis saya adalah ${this.name} ${this.breed}`);  // this.name -> old property/ inherited property
+  }
+}
+
+var anjing2 = new Dog("Harimau", 4, "Sumatera");
+anjing2.info();
