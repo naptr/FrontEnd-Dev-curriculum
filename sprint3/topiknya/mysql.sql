003e1eaa68c4acc7e733ee2ccfb606aa5db3e148
ziunadev <krishna.putra332@gmail.com>
2021-02-11 20:51:42 +0700

    update

---
+++ sprint3/topiknya/mysql.sql
@@ -0,0 +1,30 @@
+/*Create Database*/
+CREATE DATABASE database_name;
+
+/*Show existing database*/
+SHOW DATABASES;
+
+/*Entering/ Using Database*/
+USE database_name;
+
+/*Delete Database*/
+DROP DATABASE database_name;
+
+/*Create Table*/
+CREATE TABLE table_name (
+  column_one_name TYPE,
+  column_two_name TYPE,
+  PRIMARY KEY(where_to_place_primary_key)
+) ENGINE=UsedEngine;
+
+/*Show existing table*/
+SHOW TABLES;
+
+/*Delete Table*/
+DROP TABLE table_name;
+
+/*Show what's inside tables*/
+SELECT * FROM table_name; /*Asterisk sign can be replaced by column name that going to displayed*/
+
+
+/*TO BE CONTINUED ..*/
